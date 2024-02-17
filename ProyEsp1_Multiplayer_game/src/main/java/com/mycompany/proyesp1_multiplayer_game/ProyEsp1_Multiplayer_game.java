f3d992a0da56458d1c5184c1ea3984e390952302
Chango <a16001581@alumnos.uady.mx>
2024-02-17 01:32:27 -0600

    Proyecto base

---
+++ ProyEsp1_Multiplayer_game/src/main/java/com/mycompany/proyesp1_multiplayer_game/ProyEsp1_Multiplayer_game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyesp1_multiplayer_game;
+
+/**
+ *
+ * @author PC-Fredy
+ */
+public class ProyEsp1_Multiplayer_game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ProyEsp1_Multiplayer_game/src/main/java/model/Question.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC-Fredy
+ */
+public class Question {
+    private String description;
+    private ArrayList<String> answers;
+    private String correctAnswer;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+    
+    
+}
